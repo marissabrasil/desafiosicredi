08aaa2d9798e08a0010a40ed085425b18b95fb21
Marissa Brasil <marissa.brasil@gmail.com>
2023-04-04 14:27:26 -0300

    Ajustes readme

--- src/main/java/com/example/desafiosicredi/v1/controller/VotacaoController.java
+++ src/main/java/com/example/desafiosicredi/v1/controller/VotacaoController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/v1")
 public class VotacaoController {
     @Autowired
     PautaService pautaService;
